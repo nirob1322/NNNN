1ded8c32a122c8e1a2bb4b3775840284654f1bc6
nirob-kakashi66 <lal1100mia@gmail.com>
2025-07-15 01:45:48 +0600

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -51,7 +51,7 @@ module.exports = {
 
           const names = categories[category].commands.sort();
           for (let i = 0; i < names.length; i += 3) {
-            const cmds = names.slice(i, i + 2).map((item) => `⭔${item}`);
+            const cmds = names.slice(i, i + 2).map((item) => `/${item}`);
             msg += `\n│${cmds.join(" ".repeat(Math.max(1, 5 - cmds.join("").length)))}`;
           }
 
