1df38a529aa8300c1faa1518bb367cdff863e2ab
nirob-kakashi66 <lal1100mia@gmail.com>
2025-07-15 01:46:21 +0600

    Update Aniblur.js

--- scripts/cmds/Aniblur.js
+++ scripts/cmds/Aniblur.js
@@ -57,7 +57,7 @@ module.exports = {
     response.data.on("end", () => {
       api.sendMessage(
         {
-          body: "「 Here is your aniblur avatar 🥰 」\n\n author: MR᭄﹅ MAHABUB﹅ メꪜ",
+          body: "「 Here is your aniblur avatar 🥰 」\n\n author: ＮＩＲＯＢ ",
           attachment: fs.createReadStream(filePath),
         },
         event.threadID,
