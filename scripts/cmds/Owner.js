1df2ededf88dde346678edab27f76ce7d3c14e07
nirob1322 <nirobpu6@gmail.com>
2025-07-17 18:44:18 +0600

    Update Owner.js

--- scripts/cmds/Owner.js
+++ scripts/cmds/Owner.js
@@ -1,3 +1,5 @@
+const image = "https://files.catbox.moe/5wytj0.jpg";
+
 module.exports = {
   config: {
     name: "owner",
@@ -5,60 +7,37 @@ module.exports = {
     author: "NIROB",
     countDown: 5,
     role: 0,
-    shortDescription: "admin and info",
-    longDescription: "bot owner info",
+    shortDescription: "Bold style owner info",
+    longDescription: "Strong readable bold font style",
     category: "auto ✅"
   },
 
-  onStart: async function ({
-    event,
-    message,
-    getLang,
-    usersData,
-    threadsData
-  }) {
-    const userData = await usersData.get(event.senderID);
-    const userName = userData.name;
-    const threadData = await threadsData.get(event.threadID);
-    const threadName = threadData.threadName;
-
+  onStart: async function ({ event, message, usersData, threadsData }) {
+    const user = await usersData.get(event.senderID);
+    const thread = await threadsData.get(event.threadID);
     const now = new Date();
-    const dateStr = now.toLocaleDateString("en-US", {
-      year: "numeric",
-      month: "numeric",
-      day: "numeric"
-    });
-    const timeStr = now.toLocaleTimeString("en-US", {
-      timeZone: "Asia/Dhaka",
-      hour12: true
-    });
+    const date = now.toLocaleDateString("en-US", { day: "2-digit", month: "short", year: "numeric" });
+    const time = now.toLocaleTimeString("en-US", { timeZone: "Asia/Dhaka", hour: "2-digit", minute: "2-digit", hour12: true });
 
-    const infoMessage = `╔╝❮${userName}❯╚╗
-━━━━━━━━━━━━━━━━━━━━━━
-𝐍𝐀𝐌𝐄: NIROB HOSSAIN
-𝐑𝐄𝐋𝐈𝐆𝐈𝐎𝐍: ISLAM
-𝐀𝐃𝐃𝐑𝐄𝐒𝐒: Munshiganj
-𝐆𝐄𝐍𝐃𝐄𝐑: MALE
-𝐀𝐆𝐄: 18
-𝐑𝐄𝐋𝐀𝐓𝐈𝐎𝐍𝐒𝐇𝐈𝐏: 𝐅𝐀𝐈𝐋𝐄𝐃
-𝐖𝐎𝐑𝐊: NTG
-𝐆𝐌𝐀𝐈𝐋: nahadnirob@gmail.com
-𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊: https://www.facebook.com/nirob.nahad007
-𝐌𝐀𝐒𝐒𝐄𝐍𝐆𝐄𝐑: N/A
-𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏: wa.me/+8801772594397
-𝐈𝐌𝐎: PERSONAL 🫣
-𝐓𝐄𝐋𝐄𝐆𝐑𝐀𝐌: N/A
-━━━━━━━━━━━━━━━━━━━━━━
+    const info = `
+╭── ⌗ 𝗢𝘄𝗻𝗲𝗿 𝗜𝗻𝗳𝗼 ・୨୧ ──╮
 
-Bot Prefix: ( . )
-Bot Name: CAT BOT
-GC Name: ${threadName}
-Time:【 ${dateStr} || ${timeStr} 】
-━━━━━━━━━━━━━━━━━━━━━━`;
+∘ 𝗡𝗮𝗺𝗲    : 𝗡𝗶𝗿𝗼𝗯 𝗛𝗼𝘀𝘀𝗮𝗶𝗻  
+∘ 𝗪𝗣      : wa.me/8801772594397  
+∘ 𝗔𝗱𝗱𝗿𝗲𝘀𝘀 : 𝗠𝘂𝗻𝘀𝗵𝗶𝗴𝗮𝗻𝗷  
+∘ 𝗚𝗲𝗻𝗱𝗲𝗿  : 𝗠𝗮𝗹𝗲  
+∘ 𝗥𝗲𝗹𝗶𝗴𝗶𝗼𝗻: 𝗜𝘀𝗹𝗮𝗺  
+∘ 𝗔𝗴𝗲     : 𝟭𝟴  
+
+∘ 𝗕𝗼𝘁     : 𝗡 𝗜 𝗥 𝗢 𝗕 - 𝗕 𝗢 𝗧  
+∘ 𝗚𝗰      : ${thread.threadName}  
+∘ 𝗧𝗶𝗺𝗲    : ${date} | ${time}
+
+╰── ⌗ 𝗦𝘁𝗮𝘆 𝗖𝗹𝗮𝘀𝘀𝘆, 𝗦𝘁𝗮𝘆 𝗦𝗼𝗳𝘁 ──╯`;
 
     await message.reply({
-      body: infoMessage,
-      attachment: await global.utils.getStreamFromURL("https://files.catbox.moe/a86iqb.mp4")
+      body: info,
+      attachment: await global.utils.getStreamFromURL(image)
     });
   }
 };
